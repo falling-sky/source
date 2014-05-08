d89dfa67cd0d005e945668340e9da915d7f5f93c
Jason Fesler <jfesler@gigo.com>
2014-05-07 17:43:40 -0700

    Download via crowdin API. The CLI tool is failing due to the filename format being busted.

---
+++ po/download.pl
@@ -0,0 +1,26 @@
+#! /usr/bin/perl
+
+use YAML::Syck;
+use LWP::Simple;
+
+my $config = LoadFile("crowdin.yaml");
+
+my $pid = $config->{"project_identifier"};
+my $key = $config->{"api_key"};
+my $base = $config->{"base_path"};
+my $EXPORT = "http://api.crowdin.net/api/project/${pid}/export?key=${key}";
+my $DOWNLOAD = "http://api.crowdin.net/api/project/${pid}/download/all.zip?key=${key}";
+
+
+die unless (is_success(getprint($EXPORT)));
+
+my $rc = (mirror($DOWNLOAD,"all.zip"));
+if ($rc == RC_NOT_MODIFIED) {
+  print "Nothing to do!\n";
+  exit;
+} else {
+  print "rc $rc\n";
+  system("rm -fr dl && mkdir dl && cd dl && unzip ../all.zip");
+}
+
+
