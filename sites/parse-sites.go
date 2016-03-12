0ed67e0c1ae8a05ff22d52c674fa8f354197f8d5
Jason Fesler <jfesler@gigo.com>
2016-03-12 11:28:12 -0800

    write ../js/sites_parsed.js and ../js/sites_parsed_raw.js

--- sites/parse-sites.go
+++ sites/parse-sites.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync"
+	"time"
 )
+
+var input = flag.String("input", "sites.json", "json file to read")
+var parsed = flag.String("parsed", "../js/sites_parsed.js", "GIGO.sites_parsed= js file to write for falling-sky")
+var raw = flag.String("raw", "../js/sites_parsed_raw.js", "js file to write for other automation")
 
 // SiteRecord describes a single mirror or "Other Sites" record
 type SiteRecord struct {
@@ -49,6 +56,20 @@ func ReadSitesFile(fn string) (*SitesFile, error) {
 	return &sf, nil
 }
 
+func (sm *SitesMap) WriteJS(fn string, prefix string, suffix string) error {
+	if fn == "" {
+		return nil
+	}
+	s := prefix + sm.String() + suffix
+	b := []byte(s)
+	err := ioutil.WriteFile(fn+".new", b, 0755)
+	if err != nil {
+		return err
+	}
+	err = os.Rename(fn+".new", fn)
+	return err
+}
+
 // Bytes returns formatted data
 func (sf *SitesFile) Bytes() ([]byte, error) {
 	b, err := json.MarshalIndent(sf, "", " ")
@@ -61,6 +82,24 @@ func (sf *SitesFile) Bytes() ([]byte, error) {
 // String returns formatted data
 func (sf *SitesFile) String() string {
 	b, err := sf.Bytes()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(b)
+}
+
+// Bytes returns formatted data
+func (sm *SitesMap) Bytes() ([]byte, error) {
+	b, err := json.MarshalIndent(sm, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return b, err
+}
+
+// String returns formatted data
+func (sm *SitesMap) String() string {
+	b, err := sm.Bytes()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -89,27 +128,56 @@ func (sf *SitesFile) FixDefaults() error {
 	return nil
 }
 
+func (sf *SitesFile) DeleteHidden() error {
+	for key, sr := range sf.Sites {
+		if sr.Hide == "1" {
+			delete(sf.Sites, key)
+		}
+	}
+	return nil
+}
+
 func CheckHTTP(url string) error {
+
+	// Complain about slow urls
+	t0 := time.Now()
+	defer func() {
+		t1 := time.Now()
+		td := t1.Sub(t0)
+		if td > (time.Second * time.Duration(5)) {
+			log.Printf("Slow! %s %s\n", td, url)
+		}
+	}()
+
+	// Start the fetch
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
+
+	// Finish the fetch
 	_, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
+
+	// Check the content type
 	ct := resp.Header.Get("Content-Type")
 	if ct == "" {
 		return fmt.Errorf("%s: No Content-Type in response", url)
 	}
 	ctl := strings.ToLower(ct)
+
+	// If we like the content type, return success
 	if ctl == "image/gif" ||
 		ctl == "image/jpg" ||
 		ctl == "image/jpeg" ||
 		ctl == "image/png" {
 		return nil
 	}
+
+	// Otherwise complain
 	return fmt.Errorf("%s: Bad Content-Type: %s", url, ct)
 
 }
@@ -117,14 +185,22 @@ func CheckHTTP(url string) error {
 // CheckHTTP checks if a given URL is good or not;
 // and if not good, marks  SiteRecord.Hide=1
 func (sr *SiteRecord) CheckHTTP(wg *sync.WaitGroup) {
+	// Mark the goroutine as done on function exit
 	defer wg.Done()
+
+	// Try IPv4 then IPv6.
+	// Don't do these in parallel with each other;
+	// we don't want to deal with lock issues
+	// on sr.Reason .  So, do them in serial.
 	if err4 := CheckHTTP(sr.V4); err4 != nil {
 		sr.Reason = err4.Error()
+		sr.Hide = "1"
 		log.Println(sr.Reason)
 		return
 	}
 	if err6 := CheckHTTP(sr.V6); err6 != nil {
 		sr.Reason = err6.Error()
+		sr.Hide = "1"
 		log.Println(sr.Reason)
 		return
 	}
@@ -132,6 +208,8 @@ func (sr *SiteRecord) CheckHTTP(wg *sync.WaitGroup) {
 
 // CheckHTTP against the entire SitesFile
 func (sf *SitesFile) CheckHTTP() {
+	// Each site, we will spin off as a goroutine.
+	// When we do so, we need to track their finish.
 	wg := &sync.WaitGroup{}
 
 	for _, sr := range sf.Sites {
@@ -143,6 +221,10 @@ func (sf *SitesFile) CheckHTTP() {
 	wg.Wait()
 }
 
+// fixFakeBoolean takes the various types of strings that represent
+// true and false, and forces them to 0 and 1.  This is mostly legacy.
+// This can perhaps be cahnged - but I don't want to change falling-sky
+// javascript code (yet).
 func fixFakeBoolean(s string) string {
 	if s == "" || s == "0" || s[0:0] == "f" {
 		return "0"
@@ -151,12 +233,23 @@ func fixFakeBoolean(s string) string {
 }
 
 func main() {
-	sf, err := ReadSitesFile("sites.json")
+	flag.Parse()
+	sf, err := ReadSitesFile(*input)
 	if err != nil {
 		log.Fatal(err)
 	}
 	sf.FixDefaults()
 	sf.CheckHTTP()
-	sf.Print()
+
+	sf.DeleteHidden()
+
+	err = sf.Sites.WriteJS(*parsed, "GIGO.sites_parsed=", ";")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = sf.Sites.WriteJS(*raw, "", "")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
