dd07a97d1c803f07f2d9cf16b0bf1ff6eb0456af
Jason Fesler <jfesler@gigo.com>
2016-03-23 17:48:38 -0700

    jquery 1.10.2 -> 1.12.2

--- templates/js/index.js
+++ templates/js/index.js
@@ -2,7 +2,7 @@
 
 
 [% PROCESS  "inc/GIGO.js"                      %]
-[% PROCESS  "ext/jquery-1.10.2.min.js"     %]
+[% PROCESS  "ext/jquery-1.12.2.min.js"     %]
 [% PROCESS  "ext/mootools-core-1.4.5.js"   %]
 [% PROCESS  "ext/jquery.tablesorter.js"    %]
 [% PROCESS  "ext/jquery.jsonp-2.0.2.js"    %]
