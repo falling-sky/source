151b750ca575dde4927357146ce6a08ffbf28dc2
Jason Fesler <jfesler@gigo.com>
2016-06-30 00:20:30 -0700

    tweaking

--- banner.pl
+++ banner.pl
@@ -2,7 +2,10 @@
 
 $length=60;
 
+
+print "\e[1;33m\n"; # Yellow
 print banner(<>);
+print "\e[0m"; # reset
 
 sub banner {
     my (@input) = @_;
@@ -12,13 +15,11 @@ sub banner {
         $length = length if ( length > $length );
     }
     my (@output);
-    push(@output,"\e[1;33m"); # Yellow
     push( @output, join( "", ' ' x $indent, '#' x $length, "####\n" ) );
     foreach (@input) {
         push( @output, join( "", ' ' x $indent, "# ", substr( $_ . ' ' x 255, 0, $length ), " #\n" ) );
     }
     push( @output, join( "", ' ' x $indent, '#' x $length, "####\n" ) );
-    push(@output,"\e[0m"); # Reset
     return @output if (want_array);
     return join( "", @output );
 }
