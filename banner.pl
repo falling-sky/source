f4e2fcba89ca816540bbf5846675ef9d7c4e588a
Jason Fesler <jfesler@gigo.com>
2016-06-30 00:40:52 -0700

    Giving up on travis color

--- banner.pl
+++ banner.pl
@@ -1,14 +1,10 @@
 #! /usr/bin/perl
 
-$length=60;
+use strict;
 
-$BROWN = "\e[0;0;33m";
-$YELLOW = "\e[0;1;33m";
-$RESET = "\e[0;0;00m";  # Same number of characters as YELLOW
+my $length = 60;
 
-# Yellow!
 print banner(<>);
-print $RESET . "\n";
 
 sub banner {
     my (@input) = @_;
@@ -18,11 +14,10 @@ sub banner {
         $length = length if ( length > $length );
     }
     my (@output);
-    push( @output, join( "", $BROWN, ' ' x $indent, '#' x $length, "####\n" ) );
+    push( @output, join( "", '#' x $length, "####\n" ) );
     foreach (@input) {
-        push( @output, join( "", $YELLOW, ' ' x $indent, "# ", substr( $_ . ' ' x 255, 0, $length ), " #\n" ) );
+        push( @output, join( "", "# ", substr( $_ . ' ' x 255, 0, $length ), " #\n" ) );
     }
-    push( @output, join( "", $BROWN, ' ' x $indent, '#' x $length, "####\n" ) );
-    return @output if (want_array);
-    return join( "", @output );
+    push( @output, join( "", '#' x $length, "####\n" ) );
+    return @output;
 }
