932e3d85670fbcbcb99d51f0f2e17ac8015aed16
Jason Fesler <jfesler@gigo.com>
2016-06-30 00:16:02 -0700

    banner fixes, colors

--- banner.pl
+++ banner.pl
@@ -12,11 +12,13 @@ sub banner {
         $length = length if ( length > $length );
     }
     my (@output);
+    push(@output,"\e[1;33m"); # Yellow
     push( @output, join( "", ' ' x $indent, '#' x $length, "####\n" ) );
     foreach (@input) {
         push( @output, join( "", ' ' x $indent, "# ", substr( $_ . ' ' x 255, 0, $length ), " #\n" ) );
     }
     push( @output, join( "", ' ' x $indent, '#' x $length, "####\n" ) );
+    push(@output,"\e[0m"); # Reset
     return @output if (want_array);
     return join( "", @output );
 }
