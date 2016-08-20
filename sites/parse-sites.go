53ce8bfbb045ece33754061076e45042b02ece7b
Jason Fesler <jfesler@gigo.com>
2016-08-20 11:15:38 -0700

    more checks for official mirrors; clean up outputs; stop warning on slow targets if the target also has an error.

--- sites/parse-sites.go
+++ sites/parse-sites.go
@@ -18,22 +18,22 @@ var input = flag.String("input", "sites.json", "json file to read")
 var parsed = flag.String("parsed", "../templates/js/sites_parsed.js", "GIGO.sites_parsed= js file to write for falling-sky")
 var raw = flag.String("raw", "../templates/js/sites_parsed_raw.js", "js file to write for other automation")
 var validator = flag.String("validator", "http://validator.test-ipv6.com/urlvalidate.cgi", "use this service to validate a url, instead of fetching directly")
-var slow = flag.Duration("slow",time.Second * 8,"time to consider a lookup 'slow'")
-var minimumCount = flag.Int("minimum",0,"Minimum number of sites that must answer, else fail")
+var slow = flag.Duration("slow", time.Second*8, "time to consider a lookup 'slow'")
+var minimumCount = flag.Int("minimum", 0, "Minimum number of sites that must answer, else fail")
 
 // SiteRecord describes a single mirror or "Other Sites" record
 type SiteRecord struct {
-	Site     string `json:"site"`     // What site name to publicly attribute
-	Mirror   bool   `json:"mirror"`   // if true, site is a full mirror; if false, just "OtherSites"
-	Hide     bool   `json:"hide"`     // If true, stop sending traffic here
-	V4       string `json:"v4"`       // IPv4 test URL  (http or https)
-	V6       string `json:"v6"`       // IPv6 test URL (http or https)
-	Loc      string `json:"loc"`      // Country where the site is located
-	Provider string `json:"provider"` // What provider to publicly attribute
-	Monitor  string `json:"monitor"`  // Who to notify on error (automation)
-	Contact  string `json:"contact"`  // Who to notify on error (human)
-	Reason   string `json:"reason"`   // Reason disabled
-	Transparent bool `json:"transparent"` // Whether or not the site is a transparent mirror
+	Site        string `json:"site"`        // What site name to publicly attribute
+	Mirror      bool   `json:"mirror"`      // if true, site is a full mirror; if false, just "OtherSites"
+	Hide        bool   `json:"hide"`        // If true, stop sending traffic here
+	V4          string `json:"v4"`          // IPv4 test URL  (http or https)
+	V6          string `json:"v6"`          // IPv6 test URL (http or https)
+	Loc         string `json:"loc"`         // Country where the site is located
+	Provider    string `json:"provider"`    // What provider to publicly attribute
+	Monitor     string `json:"monitor"`     // Who to notify on error (automation)
+	Contact     string `json:"contact"`     // Who to notify on error (human)
+	Reason      string `json:"reason"`      // Reason disabled
+	Transparent bool   `json:"transparent"` // Whether or not the site is a transparent mirror
 }
 
 // SitesMap is a series of site records, keyed by site name
@@ -146,20 +146,22 @@ func (sf *SitesFile) CountRemaining() error {
 	}
 	c := len(sf.Sites)
 	if c < *minimumCount {
-		return fmt.Errorf("Too few sites (%v) validated, wanted %v.  Fix errors or lower --minimum.",c,*minimumCount)
+		return fmt.Errorf("Too few sites (%v) validated, wanted %v.  Fix errors or lower --minimum.", c, *minimumCount)
 	}
 	return nil
 }
 
-func CheckHTTP(urlString string) error {
+func CheckHTTP(urlString string) (E error) {
 
 	// Complain about slow urlStrings
 	t0 := time.Now()
 	defer func() {
-		t1 := time.Now()
-		td := t1.Sub(t0)
-		if td > (*slow) {
-			log.Printf("Slow! %s %s\n", td, urlString)
+		if E == nil {
+			t1 := time.Now()
+			td := t1.Sub(t0)
+			if td > (*slow) {
+				log.Printf("Slow! %s %s\n", td, urlString)
+			}
 		}
 	}()
 
@@ -193,9 +195,9 @@ func CheckHTTP(urlString string) error {
 	if err != nil {
 		return err
 	}
-	
-	if resp.StatusCode < 200 || resp.StatusCode >= 300  {
-	   return fmt.Errorf("%s: %s", urlString, resp.Status);
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return fmt.Errorf("%s: %s", urlString, resp.Status)
 	}
 
 	// Check the content type
@@ -231,14 +233,38 @@ func (sr *SiteRecord) CheckHTTP(wg *sync.WaitGroup) {
 	if err4 := CheckHTTP(sr.V4); err4 != nil {
 		sr.Reason = err4.Error()
 		sr.Hide = true
-		log.Println(sr.V4, sr.Reason)
+		log.Println(sr.Reason)
 		return
 	}
 	if err6 := CheckHTTP(sr.V6); err6 != nil {
 		sr.Reason = err6.Error()
 		sr.Hide = true
-		log.Println(sr.V6, sr.Reason)
+		log.Println(sr.Reason)
 		return
+	}
+
+}
+
+// CheckMIRROR checks if a mirror's urls are good - a subset of
+// a full verification.  TODO: Make a lib that does full site verification.
+func (sr *SiteRecord) CheckMIRROR(domain string, wg *sync.WaitGroup) {
+	// Mark the goroutine as done on function exit
+	defer wg.Done()
+
+	// Try IPv4 then IPv6.
+	// Don't do these in parallel with each other;
+	// we don't want to deal with lock issues
+	// on sr.Reason .  So, do them in serial.
+	patterns := []string{"http://ipv4.%s/images-nc/knob_green.png", "http://ipv6.%s/images-nc/knob_green.png", "http://ds.v6ns.%s/images-nc/knob_green.png", "http://mtu1280.%s/images-nc/knob_green.png"}
+
+	for _, pattern := range patterns {
+		try := fmt.Sprintf(pattern, domain)
+		if err := CheckHTTP(try); err != nil {
+			sr.Reason = err.Error()
+			sr.Hide = true
+			log.Println(sr.Reason)
+			return
+		}
 	}
 }
 
@@ -248,10 +274,14 @@ func (sf *SitesFile) CheckHTTP() {
 	// When we do so, we need to track their finish.
 	wg := &sync.WaitGroup{}
 
-	for _, sr := range sf.Sites {
+	for domain, sr := range sf.Sites {
 		if sr.Hide == false {
 			wg.Add(1)
-			go sr.CheckHTTP(wg)
+			if sr.Mirror {
+				go sr.CheckMIRROR(domain, wg)
+			} else {
+				go sr.CheckHTTP(wg)
+			}
 			time.Sleep(20 * time.Millisecond)
 		}
 	}
@@ -264,17 +294,17 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	sf.FixDefaults() // Fix the v4 and v6 test urls if blank
+	sf.FixDefaults()  // Fix the v4 and v6 test urls if blank
 	sf.DeleteHidden() // First pass cleanup, stuff humans marked hide
-	
-	log.Printf("Checking %v sites\n",len(sf.Sites))
-	
-	sf.CheckHTTP() // Do active checks against the v4 and v6 test urls
+
+	log.Printf("Checking %v sites\n", len(sf.Sites))
+
+	sf.CheckHTTP()    // Do active checks against the v4 and v6 test urls
 	sf.DeleteHidden() // Second pass cleanup, stuff CheckHTTP marked hide
-	 
-	log.Printf("After checking, %v sites remain\n",len(sf.Sites))
-	
-	if err= sf.CountRemaining(); err!=nil {
+
+	log.Printf("After checking, %v sites remain\n", len(sf.Sites))
+
+	if err = sf.CountRemaining(); err != nil {
 		log.Fatal(err)
 	}
 
