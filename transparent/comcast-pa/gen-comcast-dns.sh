6c3bc9182c58208d81ced2b1b723ed128eac8911
Jason Fesler <jfesler@Jasons-MacBook-Pro-2.local>
2017-12-21 13:58:45 -0800

    remove old way of handling transparent mirrors

--- transparent/comcast-pa/gen-comcast-dns.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-../../../extras/generate-dns-examples-for-falling-sky \
- --domain test-ipv6-pa.comcast.net \
- --hostmaster domregtech.comcastonline.com \
- --ipv4 96.119.4.224  \
- --ipv6 2001:558:fc00:200:f816:3eff:fead:9010
-
