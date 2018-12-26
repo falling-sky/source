56795bd92176b82f60806c873c53e91c2c06a403
Jason Fesler <jfesler@Jasons-MacBook-Pro-2.local>
2018-12-26 14:34:33 -0800

    use test-ipv6.com for ood check

--- templates/js/inc/ood.js
+++ templates/js/inc/ood.js
@@ -5,7 +5,7 @@ GIGO.ood_url = function () {
         hash = jQuery.md5(MirrorConfig.load.domain);
         hash = hash.replace(/[^a-zA-Z0-9]/g, '');
         hash = hash.substring(0, 8);
-        url = "//test-ipv6.com/images_" + hash + ".png";
+        url = "//test-ipv6.com/images/ood_" + hash + ".png";
     } catch (err) {
     };
     return url;
