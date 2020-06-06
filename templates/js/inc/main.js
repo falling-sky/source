af54ba30f3abdd130ec7c354c55861f3b4d51f49
Jason Fesler <jfesler@Jasons-MacBook-Pro-2.local>
2020-06-06 14:43:07 -0700

    epic contact

--- templates/js/inc/main.js
+++ templates/js/inc/main.js
@@ -631,7 +631,7 @@ GIGO.setup_tests = function () {
 
     GIGO.queue.push(["test_type_json", GIGO.options.url.test_ipv4, "test_ipv4"]);
     GIGO.queue.push(["test_type_json", GIGO.options.url.test_ipv6, "test_ipv6"]);
-    
+
     console.log("check for ssl");
     if (GIGO.CheckHTTPS()) {
         console.log("disabling test_https test_ipv4 test_ipv6 due to ssl");
