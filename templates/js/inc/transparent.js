1661103c59001054cc1f3a17f10421df3bfa42b8
jfesler <jfesler@gigo.com>
2021-10-02 11:55:45 -0700

    bias master to match domain user loaded

--- templates/js/inc/transparent.js
+++ templates/js/inc/transparent.js
@@ -30,16 +30,31 @@ GIGO.CheckTransparent = function() {
                 contact: "Jason Fesler",
                 mailto: "jfesler@test-ipv6.com"
             };
+
+            // Bias the reporting location if the user is visiting a specific flavor of the site.
+            let master = "master.test-ipv6.com"
+            if (document.location.hostname.startsWith("ipv6.")) {
+                MirrorConfig.options.show_stats = "ipv6.master.test-ipv6.com";
+            }
+            if (document.location.hostname.startsWith("ipv4.")) {
+                MirrorConfig.options.show_stats = "ipv4.master.test-ipv6.com";
+            }
+            if (document.location.hostname.startsWith("ds.")) {
+                MirrorConfig.options.show_stats = "ds.master.test-ipv6.com";
+            }
+
+
             MirrorConfig.orig_options = JSON.parse(JSON.stringify(MirrorConfig.options));
-            MirrorConfig.options.show_stats = GIGO.protocol + "master.test-ipv6.com/stats.html";
+
+            // Override the options.
+            MirrorConfig.options.show_stats = GIGO.protocol + master + "/stats.html";
             MirrorConfig.options.survey = "/survey.php";
             MirrorConfig.options.comment = "/comment.php";
             MirrorConfig.options.comment_html = 1;
             MirrorConfig.options.v6mtu = "mtu1280." + MirrorConfig.load.domain;
-            MirrorConfig.options.userdata = "master.test-ipv6.com";
+            MirrorConfig.options.userdata = master;
             MirrorConfig.facebook =  {    "enable": 0,    "fb_admins": "688631212"  };
             MirrorConfig.twitter =  {  "enable": 0,    "name": "testipv6com"};
-
         }
     }
     console.log("CheckTransparent: MirrorConfig is now %o",MirrorConfig);
