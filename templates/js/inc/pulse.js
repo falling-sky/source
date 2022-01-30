eeef86c0ccecab5769df3c26a8742ff7121bc69a
jfesler <jfesler@gigo.com>
2022-01-30 10:39:26 -0800

    dark mode, based on PR by https://github.com/mtzfederico https://github.com/falling-sky/source/pull/191

--- templates/js/inc/pulse.js
+++ templates/js/inc/pulse.js
@@ -2,18 +2,41 @@
 cycling through colors ranging from yellow to white */
 
 GIGO.pulse_background_speed = 250;
-GIGO.pulse_background_colors = [
-"#fffeee",
-"#fffeee",
-"#fffddd",
-"#fffccc",
-"#fffbbb",
-'#fffbbb',
-"#fffccc",
-"#fffddd",
-"#fffeee",
-"#fffeee"];
+GIGO.pulse_background_colors_function = function () {
+ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  // dark mode
+  return [
+   "#3b3528",
+   "#59503c",
+   "#786c50",
+   "#968150",
+   "#968150",
+   "#786c50",
+   "#59503c",
+   "#3b3528"];
+ } else {
+  return  [
+   "#fffeee",
+   "#fffeee",
+   "#fffddd",
+   "#fffccc",
+   "#fffaaa",
+   "#ffe6aa",
+   "#fffaaa",
+   "#fffccc",
+   "#fffddd",
+   "#fffeee",
+   "#fffeee"];
+ }
+}
 
+
+GIGO.pulse_background_colors = GIGO.pulse_background_colors_function();
+
+// On change, refresh the colors.
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+ GIGO.pulse_background_colors = GIGO.pulse_background_colors_function();
+});
 
 GIGO.pulse_on = function () {
  var n = GIGO.pulse_background_colors.shift();
